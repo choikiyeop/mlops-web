85c329187df50a54c131ce92c7f8d19c9396543b
choikiyeop <cky9709@naver.com>
2024-09-12 11:26:17 +0900

    feat: total-dashboard 생성

---
+++ apps/total-dashboard/tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from 'tailwindcss';
+import sharedConfig from '@repo/tailwind-config/tailwind.config';
+
+const config: Config = {
+  content: [
+    '../../packages/ui/src/**/*.{ts,tsx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+    },
+  },
+  plugins: [],
+  presets: [sharedConfig],
+};
+export default config;
