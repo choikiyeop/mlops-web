3c16d42d7a661d1096d464c9d305abefa23ff544
choikiyeop <cky9709@naver.com>
2024-09-19 16:20:37 +0900

    fix(ui): dashboard-layout 수정

--- packages/ui/src/dashboard-layout/dashboard-layout.tsx
+++ packages/ui/src/dashboard-layout/dashboard-layout.tsx
@@ -5,8 +5,8 @@ interface DashboardLayoutProps extends HTMLAttributes<HTMLDivElement> {}
 
 export const DashboardLayout = ({ className, children, ...props }: DashboardLayoutProps) => {
   return (
-    <div className={cn('bg-background max-h-screen flex-1 pt-1', className)} {...props}>
-      <div className="bg-foreground size-full rounded-tl-[7px] pl-2.5">{children}</div>
+    <div className={cn('bg-background flex flex-1 flex-col overflow-y-auto', className)} {...props}>
+      {children}
     </div>
   );
 };
@@ -15,20 +15,22 @@ interface DashboardTitleProps extends HTMLAttributes<HTMLHeadingElement> {}
 
 export const DashboardTitle = ({ className, children, ...props }: DashboardTitleProps) => {
   return (
-    <div
-      className={cn(
-        "flex h-16 items-center pl-[9px] before:mr-[14px] before:h-4 before:border-l-[3px] before:border-solid before:border-[#071e3f] before:content-['']",
-        className
-      )}
-      {...props}
-    >
-      <h1 className="text-lg">{children}</h1>
+    <div className={cn('bg-background sticky top-0', className)} {...props}>
+      <div className="h-1" />
+      <div className="bg-foreground flex h-16 items-center rounded-tl-[7px] pl-[19px] before:mr-[14px] before:h-4 before:border-l-[3px] before:border-solid before:border-[#071e3f] before:content-['']">
+        <h1 className="text-lg">{children}</h1>
+      </div>
     </div>
   );
 };
 
 interface DashboardContentProps extends HTMLAttributes<HTMLDivElement> {}
 
-export const DashboardContent = ({ children, ...props }: DashboardContentProps) => {
-  return <div {...props}>{children}</div>;
+export const DashboardContent = ({ className, children, ...props }: DashboardContentProps) => {
+  return (
+    <div className={cn('bg-foreground flex-grow px-2.5', className)} {...props}>
+      <div className="border-t border-t-[#071e3f]" />
+      <div className="border-border border-x bg-white">{children}</div>
+    </div>
+  );
 };
