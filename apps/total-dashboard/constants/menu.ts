f501bfef1767584954e042ae98f9dbe387558fbe
choikiyeop <cky9709@naver.com>
2024-09-23 08:37:32 +0900

    fix: 메뉴 href 변경

--- apps/total-dashboard/constants/menu.ts
+++ apps/total-dashboard/constants/menu.ts
@@ -6,155 +6,155 @@ export const MENUS: Record<MenuType, Menu[]> = {
   SYSTEM_MENU: [
     {
       name: '글로벌 대시보드',
-      href: 'global-dashboard',
+      href: '/global-dashboard',
     },
     {
       name: '클러스터',
-      href: 'cluster',
+      href: '/cluster',
     },
     {
       name: '프로젝트',
-      href: 'project',
+      href: '/project',
     },
     {
       name: '사용자',
-      href: 'user',
+      href: '/user',
     },
     {
       name: '그룹',
-      href: 'group',
+      href: '/group',
     },
     {
       name: '헬름 저장소',
-      href: 'helm-repo',
+      href: '/helm-repo',
     },
     {
       name: '감사 로그',
-      href: 'audit-log',
+      href: '/audit-log',
     },
   ],
   CLUSTER_MENU: [
     {
       name: '대시보드',
-      href: 'dashboard',
+      href: '/dashboard',
     },
     {
       name: '시스템',
-      href: 'system',
+      href: '/system',
       children: [
         {
           name: '노드',
-          href: 'node',
+          href: '/node',
         },
         {
           name: '네임스페이스',
-          href: 'namespace',
+          href: '/namespace',
         },
         {
           name: '리소스 쿼터',
-          href: 'resource-quota',
+          href: '/resource-quota',
         },
         {
           name: '리밋 레인지',
-          href: 'limit-range',
+          href: '/limit-range',
         },
       ],
     },
     {
       name: '모니터링',
-      href: 'monitoring',
+      href: '/monitoring',
       children: [
         {
           name: '모니터링 대시보드',
-          href: 'monitoring-dashboard',
+          href: '/monitoring-dashboard',
         },
         {
           name: '그래프',
-          href: 'graph',
+          href: '/graph',
         },
       ],
     },
     {
       name: '로깅',
-      href: 'logging',
+      href: '/logging',
     },
     {
       name: '카탈로그',
-      href: 'catalog',
+      href: '/catalog',
       children: [
         {
           name: '노트북 이미지 목록',
-          href: 'notebook-image',
+          href: '/notebook-image',
         },
         {
           name: '헬름 차트',
-          href: 'helm-chart',
+          href: '/helm-chart',
         },
         {
           name: '헬름 릴리즈',
-          href: 'helm-release',
+          href: '/helm-release',
         },
       ],
     },
     {
       name: '스토리지',
-      href: 'storage',
+      href: '/storage',
       children: [
         {
           name: '볼륨',
-          href: 'volume',
+          href: '/volume',
         },
         {
           name: '볼륨 클레임',
-          href: 'volume-claim',
+          href: '/volume-claim',
         },
         {
           name: '스토리지 클래스',
-          href: 'storage-class',
+          href: '/storage-class',
         },
       ],
     },
     {
       name: 'Yaml',
-      href: 'yaml',
+      href: '/yaml',
     },
     {
       name: '프로필',
-      href: 'profile',
+      href: '/profile',
     },
     {
       name: 'AI 개발환경',
-      href: 'ai-enviorment',
+      href: '/ai-enviorment',
     },
     {
       name: 'tensorboard',
-      href: 'tensorboard',
+      href: '/tensorboard',
     },
     {
       name: 'AutoML',
-      href: 'automl',
+      href: '/automl',
     },
     {
       name: 'pipeline',
-      href: 'pipeline',
+      href: '/pipeline',
     },
     {
       name: '모델 카탈로그',
-      href: 'model-catalog',
+      href: '/model-catalog',
       children: [
         {
           name: '모델 카탈로그',
-          href: 'model-catalog',
+          href: '/model-catalog',
         },
         {
           name: '서드파티 모델 마켓',
-          href: 'thirdparty-modelmarket',
+          href: '/thirdparty-modelmarket',
         },
       ],
     },
     {
       name: '데이터레이크',
-      href: 'datalake',
+      href: '/datalake',
     },
   ],
 };
