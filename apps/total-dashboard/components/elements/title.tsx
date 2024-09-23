114c4040e216e37de6c50c7713082ac45c330c76
choikiyeop <cky9709@naver.com>
2024-09-23 14:42:20 +0900

    refactor: useMemo로 리팩터링

--- apps/total-dashboard/components/elements/title.tsx
+++ apps/total-dashboard/components/elements/title.tsx
@@ -2,17 +2,14 @@
 
 import { usePathname } from 'next/navigation';
 import { MENUS } from '../../constants/menu';
+import { useMemo } from 'react';
 
 export const Title = () => {
   const pathname = usePathname();
 
-  return (
-    <>
-      {
-        MENUS.CLUSTER_MENU.flatMap((item) => (item.children ? item.children : item)).find(
-          (menu) => menu.href === pathname
-        )?.name
-      }
-    </>
-  );
+  const menus = useMemo(() => {
+    return MENUS.CLUSTER_MENU.flatMap((item) => (item.children ? item.children : item));
+  }, []);
+
+  return <>{menus.find((menu) => menu.href === pathname)?.name}</>;
 };
